c65bf16d3ab2577742a2a36fa3ff94107f56925f
Daniel2936 <danielvillamizar72@gmail.com>
2023-04-11 13:38:22 -0500

    dios

---
+++ components/cardpokemon.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="w-[20%] bg-red-200">
+        <!-- <img src="/logo coex.jpg" alt=""> -->
+        <img :src="url" alt="">
+        <p class="text-center text-2xl font-bold">{{ nombre }}</p>
+    </div>
+</template>
+    
+<script>
+
+export default {
+    name: 'Cardcomponent',
+    props:{
+        nombre:{
+            type:String,
+            requiered:true
+        },
+        url:{
+            type:String,
+            requiered:true
+        }
+    }
+
+  }
+    
+</script>
+    
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,39 @@
 <template>
-  <Tutorial />
+ 
+ <div class="h-screen">
+  <div class="w-[80%] rounded-lg p-[2%] bg-amber-100 border-2">
+    <p class="flex justify-center items-center font-semibold text-6xl">PokeDex</p>
+  </div>
+  <!-- <cardpokemon v-for="i in 10" :key="i"/> -->
+  <div class="grid grid-cols-3 w-[100%]">
+    <cardpokemon v-for="i in response" :key="i" :nombre="i.name" :url="i.url"/>
+  </div>
+
+ 
+ 
+ </div>
+
+
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  data(){
+    return{
+      response:[ ]
+    }
+  },
+  mounted(){
+    this.getInfo()
+  },
+  methods:{
+     async getInfo(){
+      const {data}= await this.$axios.get('pokemon')
+      this.response=data.results
+      console.log(data)
+    }
+  }
+
 }
 </script>
